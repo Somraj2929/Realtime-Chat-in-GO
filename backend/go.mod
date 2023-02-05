45de85292c170ac8cabb9a752d6155f7e3800767
Somraj <somrajbishnoirock29@gmail.com>
2023-02-06 04:02:40 +0530

    first commit

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/somraj2929/react-go-chat
+
+go 1.16
+
+require github.com/gorilla/websocket v1.5.0
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+  <div className="header">
+    <div className="image">
+      <img
+        src="https://i0.wp.com/letschat.org.uk/wp-content/uploads/2021/01/Lets-Chat-col-logo-2.png?fit=1134%2C524&ssl=1"
+        alt="lets-chat"
+      />
+    </div>
+
+    <h2>Chat App using React & GO</h2>
+  </div>
+);
+
+export default Header;
