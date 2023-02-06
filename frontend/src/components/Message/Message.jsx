5006d9a9dd19051f5be80b399459e0a1fe6ac3e1
Somraj <somrajbishnoirock29@gmail.com>
2023-02-06 22:30:45 +0530

    backend containerized

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,23 +1,18 @@
-import React, { Component } from 'react';
-import './Message.scss';
+import React, { Component } from "react";
+import "./Message.scss";
 
 class Message extends Component {
   constructor(props) {
     super(props);
     let temp = JSON.parse(this.props.message);
     this.state = {
-      message: temp
-    }
+      message: temp,
+    };
   }
-  
-  render() {
-    return (
-      <div className='Message'>
-        {this.state.message.body}
-      </div>
-    );
-  };
 
+  render() {
+    return <div className="Message">{this.state.message.body}</div>;
+  }
 }
 
 export default Message;
